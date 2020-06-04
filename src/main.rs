0e80ae4afd7acd7971520104fd0166a2338134d8
Ian Page Hands <iphands@gmail.com>
2020-06-04 14:53:43 -0400

    let the user pick the assets location and desc of input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate clap;
 extern crate libpulse_binding as pulse;
+
+use clap::{Arg, App};
 
 use std::rc::Rc;
 use std::cell::RefCell;
@@ -17,9 +20,36 @@ use pulse::context::subscribe::Operation;
 use pulse::context::subscribe::Facility;
 
 fn main() {
-    let (app, icons) = start_app();
-    do_mic(&app, &icons);
-    do_event_loop(app, icons);
+    let (desc, assets) = do_cli();
+    let (tray_app, icons) = start_app(assets);
+    do_mic(&tray_app, &icons, &desc);
+    do_event_loop(tray_app, icons, desc);
+}
+
+fn do_cli() -> (String, String) {
+    let matches = App::new("rust_tray")
+        .version("0.1.0")
+        .author("Ian Page Hands <iphands@gmail.com>")
+        .about("Tray icons and feedback written in Rust")
+        .arg(Arg::with_name("DESC")
+             .value_name("input description substring")
+             .short("d")
+             .long("input-description-substring")
+             .required(true)
+             .takes_value(true)
+             .help("Description: HD Webcam C615"))
+        .arg(Arg::with_name("ASSETS")
+             .value_name("path to assets")
+             .short("a")
+             .long("assets-path")
+             .takes_value(true)
+             .help("/opt/rust_tray/assets"))
+        .get_matches();
+
+    let desc = matches.value_of("DESC").unwrap().to_string();
+    let assets = matches.value_of("ASSETS").unwrap_or("/opt/rust_tray/assets").to_string();
+
+    return (desc, assets);
 }
 
 fn get_pactl_data() -> String {
@@ -32,9 +62,9 @@ fn get_pactl_data() -> String {
     return String::from_utf8_lossy(&output.stdout).to_string();
 }
 
-fn find_mic_status(pactl_data: String) -> bool {
+fn find_mic_status(pactl_data: String, description_substring: &String) -> bool {
     for (num, line) in pactl_data.lines().enumerate() {
-        if line.contains("Description: HD Webcam C615") {
+        if line.contains(description_substring) {
             // println!("{}: {}", num, line);
             // println!("{}", pactl_data.lines().nth(num + 5).unwrap());
             if pactl_data.lines().nth(num + 5).unwrap().contains("Mute: yes") {
@@ -47,7 +77,7 @@ fn find_mic_status(pactl_data: String) -> bool {
     return true;
 }
 
-fn start_app() -> (systray::Application, std::vec::Vec<String>) {
+fn start_app(assets: String) -> (systray::Application, std::vec::Vec<String>) {
     let app;
 
     match systray::Application::new() {
@@ -56,16 +86,16 @@ fn start_app() -> (systray::Application, std::vec::Vec<String>) {
     }
 
     let icons = vec![
-        "/home/iphands/prog/rust/rust_tray/assets/mic_red.png".to_string(),
-        "/home/iphands/prog/rust/rust_tray/assets/mic_green.png".to_string()
+        format!("{}{}", assets, "/mic_red.png").to_string(),
+        format!("{}{}", assets, "/mic_green.png").to_string()
     ];
 
     return (app, icons);
 }
 
-fn do_mic(app: &systray::Application, icons: &std::vec::Vec<String>) {
+fn do_mic(app: &systray::Application, icons: &std::vec::Vec<String>, description_substring: &std::string::String) {
     // let now = Instant::now();
-    let state = find_mic_status(get_pactl_data());
+    let state = find_mic_status(get_pactl_data(), description_substring);
     // println!("{}", now.elapsed().as_millis());
 
     if state {
@@ -76,15 +106,15 @@ fn do_mic(app: &systray::Application, icons: &std::vec::Vec<String>) {
     app.set_icon_from_file(&icons[0]).unwrap();
 }
 
-fn do_event_loop(app: systray::Application, icons: std::vec::Vec<String>) {
+fn do_event_loop(app: systray::Application, icons: std::vec::Vec<String>, description_substring: std::string::String) {
     let mut proplist = Proplist::new().unwrap();
-    proplist.set_str(pulse::proplist::properties::APPLICATION_NAME, "FooApp").unwrap();
+    proplist.set_str(pulse::proplist::properties::APPLICATION_NAME, "rust_tray").unwrap();
 
     let mainloop = Rc::new(RefCell::new(Mainloop::new().expect("Failed to create mainloop")));
 
     let context = Rc::new(RefCell::new(Context::new_with_proplist(
         mainloop.borrow().deref(),
-        "FooAppContext",
+        "RustTrayContext",
         &proplist
     ).expect("Failed to create new context")));
 
@@ -113,14 +143,14 @@ fn do_event_loop(app: systray::Application, icons: std::vec::Vec<String>) {
 
     let interest = subscription_masks::SOURCE;
 
-    context.borrow_mut().set_subscribe_callback(Some(callback(app, icons)));
+    context.borrow_mut().set_subscribe_callback(Some(callback(app, icons, description_substring)));
     context.borrow_mut().subscribe(interest, |_| {});
 
     mainloop.borrow_mut().run().unwrap();
     mainloop.borrow_mut().quit(Retval(0));
 }
 
-fn callback(app: systray::Application, icons: std::vec::Vec<String>) -> Box<dyn FnMut(Option<Facility>, Option<Operation>, u32)> {
+fn callback(app: systray::Application, icons: std::vec::Vec<String>, description_substring: String) -> Box<dyn FnMut(Option<Facility>, Option<Operation>, u32)> {
     Box::new(move |facility_unsafe: Option<Facility>, operation_unsafe: Option<Operation>, _idx: u32| {
         match facility_unsafe {
             None => { eprintln!("Invalid facility received from PA"); }
@@ -128,7 +158,7 @@ fn callback(app: systray::Application, icons: std::vec::Vec<String>) -> Box<dyn 
                 match operation_unsafe {
                     None => { eprintln!("Invalid operation received from PA"); }
                     Some(_) => {
-                        do_mic(&app, &icons);
+                        do_mic(&app, &icons, &description_substring);
                     }
                 }
             }
